aee3ccd0bae6cff6090c3b5afa7ccf49b60f55d6
Vinicius Nascimento <vinninascimento1305@gmail.com>
2024-08-15 23:47:16 -0300

    finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
+import { Location } from './types/location.interface';
+import { GetUnitsService } from './services/get-units.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-smartfit';
+  showList = new BehaviorSubject(false);
+  unitsList: Location[] = [];
+
+  constructor(private unitService: GetUnitsService){ }
+
+  onSubmit(){
+    this.unitsList = this.unitService.getFilteredUnits();
+    this.showList.next(true);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,25 @@ import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LegendComponent } from './components/legend/legend.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormsComponent
+    FormsComponent,
+    CardListComponent,
+    CardComponent,
+    FooterComponent,
+    LegendComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule, 
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
