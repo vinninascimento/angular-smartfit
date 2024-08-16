727c15693d1d90046656fdb9036b42db6f61779f
Vinicius Nascimento <vinninascimento1305@gmail.com>
2024-08-15 23:42:53 -0300

    card-list

---
+++ src/app/components/card-list/card-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { GetUnitsService } from 'src/app/services/get-units.service';
+import { Location } from 'src/app/types/location.interface';
+
+@Component({
+  selector: 'app-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.scss']
+})
+export class CardListComponent {
+  @Input() unitsList: Location[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
